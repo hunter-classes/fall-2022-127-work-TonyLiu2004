8c1be07fed7fe767970cde9e1158897154c520c9
TonyLiu2004 <104866433+TonyLiu2004@users.noreply.github.com>
2022-08-29 13:36:32 +0000

    Added some variables

--- Intro/intro.py
+++
@@ -1,6 +0,0 @@
-#This is my first python program
-
-print("Hello World")
-
-userName = input("What is you name?\n")
-print("Hello",userName+".")
---
+++ intro/intro.py
@@ -0,0 +1,9 @@
+#This is my first python program
+
+print("Hello World")
+
+a=3
+b=a
+print("a:",a,"b:",b)
+a=25
+print("a:",a,"b:")
