48dc89a3681a41baa2c8887be09ac9fb0d21b7ff
TonyLiu2004 <104866433+TonyLiu2004@users.noreply.github.com>
2022-09-08 13:43:09 +0000

    Updated turtle functions again

--- turtles/t2.py
+++ turtles/t2.py
@@ -14,27 +14,41 @@ def square(t,x,y,w,color,sidelen):
     t.forward(sidelen)
     t.right(90)
 
+def triangle(t,x,y,w,color,sidelen):
+  t.penup()
+  t.goto(x,y)
+  t.width(w)
+  t.color(color)
+  t.pendown()
+  for i in range(3):
+    t.forward(sidelen)
+    t.left(120)
+
+
+def ngon(t,x,y,w,color,sidelen,sides):
+  t.penup()
+  t.goto(x,y)
+  t.width(w)
+  t.color(color)
+  t.pendown()
+  for i in range(sides):
+    t.forward(sidelen)
+    t.right(360/sides)
   
 wn = turtle.Screen()
 
 crash=turtle.Turtle()
 square(crash,0,0,3,"blue",50)
 
-squirt = turtle.Turtle()
-squirt.color("red")
-squirt.width(5)
-squirt.penup()
-squirt.left(180)
-squirt.forward(50)
-squirt.left(90)
-squirt.forward(50)
-squirt.right(90)
-squirt.pendown()
-for x in range(0, 4):
-    squirt.forward(50)
-    squirt.right(120)
-
+squirt=turtle.Turtle()
 square(squirt,50,50,3,"red",80)
+
+tri=turtle.Turtle()
+triangle(tri,-50,-50,3,"yellow",50)
+
+gon=turtle.Turtle()
+ngon(gon,0,50,3,"green",50,8)
+
 wn.exitonclick()
 wn.mainloop()
 
