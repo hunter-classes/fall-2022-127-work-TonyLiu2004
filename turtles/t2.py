016c4624534e0ddf3933262f40a9b1b711396775
TonyLiu2004 <104866433+TonyLiu2004@users.noreply.github.com>
2022-09-08 13:12:06 +0000

    Updated turtle function

--- turtles/t2.py
+++ turtles/t2.py
@@ -4,17 +4,21 @@ def sample_function():
   print("this is a function")
   print("It can be ran multiple times")
 
-def square(t):
-
-  for z in range(0, 5):
-    t.forward(50)
+def square(t,x,y,w,color,sidelen):
+  t.penup()
+  t.goto(x,y)
+  t.width(w)
+  t.color(color)
+  t.pendown()
+  for z in range(4):
+    t.forward(sidelen)
     t.right(90)
 
   
 wn = turtle.Screen()
 
 crash=turtle.Turtle()
-square(crash)
+square(crash,0,0,3,"blue",50)
 
 squirt = turtle.Turtle()
 squirt.color("red")
@@ -30,7 +34,7 @@ for x in range(0, 4):
     squirt.forward(50)
     squirt.right(120)
 
-square(squirt)
+square(squirt,50,50,3,"red",80)
 wn.exitonclick()
 wn.mainloop()
 
