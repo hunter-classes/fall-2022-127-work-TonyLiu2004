ba1b0af75d29b86b88efa2689f7b6cd0750b4bc5
TonyLiu2004 <104866433+TonyLiu2004@users.noreply.github.com>
2022-10-31 13:48:45 +0000

    finished fastMode.py in mode

--- mode/modes.py
+++ mode/modes.py
@@ -1,5 +1,7 @@
 import random
 import datetime
+
+
 def findLargest(l):
     i = l[0]
     for x in l[1:]:
@@ -62,10 +64,29 @@ def testMode(size, maxValue):
     print("size: ", size, " time: ", elapsed)
 
 
+def fastMode(dataset):
+    # assume all values in dataset
+    # are between 0 and 99 inclusive
+
+    # 1. make a list of 100 slots
+    # and set them all to 0
+    # this will store our tallies
+    list = [0] * 100
+    # 2. Loop through our dataset
+    # and for each item incremement
+    # (add 1) to the appropriate
+    # slot in the tallies list
+    for x in dataset:
+        list[x] += 1
+        # 3. the index with the highest
+        # value in tallies is the mode
+    return max(list)
+
+
 list = [1, 2, 34, 23, 2, 12, 2]
 list2 = [1, 2, 3, 12, 1, 3, 2, 12, 5, 3, 12, 1]
 #print(findLargest(list))
 #print(freq(list, 2))
 #print(mode(list2))
 #print(modeEasy(list2))
-print(testMode(20000,10))
+print(fastMode(list2))
