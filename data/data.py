b927cbb2ef57b19a6c1b804ee493e6672db23152
TonyLiu <ltsplice@gmail.com>
2022-11-29 12:57:31 -0500

    updated data

--- data/data.py
+++ data/data.py
@@ -39,19 +39,68 @@ del avgStats["gameId"] #Don't need game ID
 for x in avgStats:
   print(x,avgStats[x])
 
+avgStats["redWins"] = 1-avgStats["blueWins"] #redWins not included because it can be inferred from blueWins since there are only 2 teams
+#plotDict = avgStats.copy()
+#del plotDict["blueTotalGold"]
+#del plotDict["blueTotalExperience"]
+#del plotDict["redTotalGold"]
+#del plotDict["redTotalExperience"]
+#del plotDict["blueGoldPerMin"]
+#del plotDict["redGoldPerMin"]
+#del plotDict["blueTotalMinionsKilled"]
+#del plotDict["redTotalMinionsKilled"]
+#plotDict["redWins"] = 1- plotDict["blueWins"]
+#print("--------------------")
+#print(plotDict)
 
+plotDict = {}
+unused = {}
+for x in avgStats.keys():
+  if("blue" in x):
+    plotDict[x] = avgStats[x]
+    temp = x.replace("blue","red")
+    plotDict[temp] = avgStats[temp]
 
-plotDict = avgStats.copy()
-del plotDict["blueTotalGold"]
-del plotDict["blueTotalExperience"]
-del plotDict["redTotalGold"]
-del plotDict["redTotalExperience"]
-del plotDict["blueGoldPerMin"]
-del plotDict["redGoldPerMin"]
-del plotDict["blueTotalMinionsKilled"]
-del plotDict["redTotalMinionsKilled"]
-plt.title("LOL Average Stats Blue vs Red (10000 games)")
-plt.xlabel("Stats")
+print("------------End of Statistics--------------")
+
+#deleting information that is too large for graph and appends them to unused
+toBeDeleted = []
+for keys in plotDict.keys():
+  for doNotInclude in ["TotalGold","TotalExperience","GoldPerMin","TotalMinionsKilled","JungleMinionsKilled","ExperienceDiff","CSPerMin","WardsPlaced","GoldDiff"]:
+    if doNotInclude in keys:
+      unused[keys] = plotDict[keys]
+      toBeDeleted.append(keys)
+for x in toBeDeleted:
+  del plotDict[x]
+
+#plt.title("LOL Average Stats Blue vs Red (10000 games)")
+#plt.xlabel("Stats")
+#plt.xticks(rotation = 90)
+#plt.bar(plotDict.keys(),plotDict.values(),color=['blue','red'])
+#plt.show()
+
+#testing 1
+#figure, axis = plt.subplots(2)
+#axis[0].set_title("g1")
+#axis[0].bar(plotDict.keys(),plotDict.values(),color=['blue','red'])
+#axis[0].xticks(rotation = 90)
+#axis[1].set_title("g2")
+#axis[1].bar(unused.keys(),unused.values(),color = ['blue','red'])
+#axis[1].xticks(rotation = 90)
+#plt.show()
+#end testing 1
+
+#testing 2
+plot1 = plt.subplot2grid((1,4),(0,0),colspan=2,rowspan=1) # add rotation here?
+plot2 = plt.subplot2grid((1,4),(0,2),colspan=2,rowspan=1)
+
+plot1.bar(plotDict.keys(),plotDict.values(),color = ['blue','red'])
+plot1.set_title("g1")
+plot1.set_xticklabels(plotDict.keys(),rotation = 90)
+
+plot2.bar(unused.keys(),unused.values(),color = ['blue','red'])
+plot2.set_title("g2")
+plot2.set_xticklabels(unused.keys(),rotation = 90)
+
 plt.xticks(rotation = 90)
-plt.bar(plotDict.keys(),plotDict.values(),color=['blue','red'])
 plt.show()
